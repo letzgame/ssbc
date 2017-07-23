4a6701535329bbf831ac05ffbc81975fc9ddffb0
Taylor <taylor@macpro.com>
2017-07-24 04:19:01 +0800

    fix

--- bin/ssbc-reboot.sh
+++ bin/ssbc-reboot.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#add this to crontab: 0,15 * * * * sh /root/ssbc-master/bin/ssbc-reboot.sh
+#add this to crontab: 0,15 * * * * sh /root/ssbc/bin/ssbc-reboot.sh
 ps -ef|grep simdht_worker.py|grep -v grep|awk '{print $2}'|xargs kill -9
 ps -ef|grep index_worker.py|grep -v grep|awk '{print $2}'|xargs kill -9
-cd /root/ssbc-master/workers
+cd /root/ssbc/workers
 nohup python simdht_worker.py >/dev/zero 2>&1&
 nohup python index_worker.py >/dev/zero 2>&1&
 exit
